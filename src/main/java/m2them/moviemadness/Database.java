b68114922b8ba5fc0b17144932f0384cc13531f9
SanjayBheenuck <48724324+SanjayBheenuck@users.noreply.github.com>
2019-04-16 17:11:43 +0100

    added database connection method

--- src/main/java/m2them/moviemadness/Database.java
+++ src/main/java/m2them/moviemadness/Database.java
@@ -1,10 +1,33 @@
 package m2them.moviemadness;
 
+import org.sqlite.SQLiteConfig;
+
 import java.util.ArrayList;
+
+import java.sql.*;
+
+import static java.lang.Class.forName;
+
 
 public class Database {
 
+//Author Sanjay
 
+    private Connection connectDB(){
+            Connection con = null;
+        try{
+            class.forName("org.sqlite.JDBC");
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            con = DriverManager.getConnection("jdbc:sqlite:"+"data/MoviesDatabase.db");
+            config.toProperties();
+        } catch (Exception ex){
+            System.out.println(ex.getClass());
+            ex.printStackTrace();
+        }
+
+        return con;
+    }
 
 
     public static ArrayList<Actor> getActors() {

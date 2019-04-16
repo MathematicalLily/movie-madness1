bfbf2a1d39eb2c3911f0c6c85f1cd531266e1e80
Tom Michiels <tmichiels@gmail.com>
2019-04-16 10:10:47 +0100

    db class changes

--- src/main/java/m2them/moviemadness/Database.java
+++ src/main/java/m2them/moviemadness/Database.java
@@ -4,29 +4,23 @@ import java.util.ArrayList;
 
 public class Database {
 
-    static ArrayList<Actor> actors = new ArrayList<>();
-    static ArrayList<Movie> movies = new ArrayList<>();
-    static ArrayList<Cast> casts = new ArrayList<>();
-    static ArrayList<User> users = new ArrayList<>();
-    static ArrayList<Review> reviews = new ArrayList<>();
-
     public static ArrayList<Actor> getActors() {
-        return actors;
+
     }
 
     public static ArrayList<Movie> getMovies() {
-        return movies;
+
     }
 
     public static ArrayList<Cast> getCasts() {
-        return casts;
+
     }
 
     public static ArrayList<User> getUsers() {
-        return users;
+
     }
 
     public static ArrayList<Review> getReviews() {
-        return reviews;
+
     }
 }

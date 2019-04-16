50736c96b7e1f68c408455da2ec510b0119a1758
Tom Michiels <tmichiels@gmail.com>
2019-04-16 16:16:12 +0100

    skeleton update

--- src/main/java/m2them/moviemadness/Database.java
+++ src/main/java/m2them/moviemadness/Database.java
@@ -5,22 +5,22 @@ import java.util.ArrayList;
 public class Database {
 
     public static ArrayList<Actor> getActors() {
-
+        return new ArrayList<Actor>();
     }
 
     public static ArrayList<Movie> getMovies() {
-
+        return new ArrayList<Movie>();
     }
 
     public static ArrayList<Cast> getCasts() {
-
+        return new ArrayList<Cast>();
     }
 
     public static ArrayList<User> getUsers() {
-
+        return new ArrayList<User>();
     }
 
     public static ArrayList<Review> getReviews() {
-
+        return new ArrayList<Review>();
     }
 }

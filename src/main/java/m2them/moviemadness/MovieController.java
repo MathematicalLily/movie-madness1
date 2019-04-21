6501f34875071517e9a78c95496504f0417abf28
Eric <eric@Erics-MBP.home>
2019-04-21 09:49:52 +0100

    First Commit

--- src/main/java/m2them/moviemadness/MovieController.java
+++ src/main/java/m2them/moviemadness/MovieController.java
@@ -1,7 +1,26 @@
 package m2them.moviemadness;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class MovieController {
-}
+
+    @GetMapping("/index")
+    public String moviesIndex(Model model){
+        ArrayList<Movie> movies = Database.getMovies(Database.connectDB());
+        model.addAttribute("movies", movies);
+        return "index";
+        }
+
+    }
+
+
+
+
+

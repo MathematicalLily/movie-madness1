3d0a6ee611f4908f7387f2f4d341746995472f6e
Tom Michiels <tmichiels@gmail.com>
2019-04-22 14:35:33 +0100

    switch on sort types

--- src/main/java/m2them/moviemadness/MovieController.java
+++ src/main/java/m2them/moviemadness/MovieController.java
@@ -1,10 +1,10 @@
 package m2them.moviemadness;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -16,7 +16,26 @@ public class MovieController {
         ArrayList<Movie> movies = Database.getMovies(Database.connectDB());
         movies.sort(Movie.titleComparator);
         model.addAttribute("movies", movies);
+        model.addAttribute("sortType", new SortType(1));
         return "index";
+        }
+
+        @RequestMapping("/")
+        public String movieSort(Model model, @ModelAttribute("sortType") SortType sortType) {
+            ArrayList<Movie> movies = Database.getMovies(Database.connectDB());
+            switch (sortType.getSortType()) {
+                case 1:
+                    movies.sort(Movie.titleComparator);
+                    break;
+                case 2:
+                    movies.sort(Movie.titleComparator.reversed());
+                    break;
+                case 3:
+                    movies.sort(Movie.releaseComparator);
+                    break;
+            }
+            model.addAttribute("movies", movies);
+            return "index";
         }
 
     }
---
+++ src/main/java/m2them/moviemadness/SortType.java
@@ -0,0 +1,18 @@
+package m2them.moviemadness;
+
+public class SortType {
+
+    private int sortType;
+
+    public int getSortType() {
+        return sortType;
+    }
+
+    public void setSortType(int sortType) {
+        this.sortType = sortType;
+    }
+
+    public SortType(int sortType) {
+        this.sortType = sortType;
+    }
+}

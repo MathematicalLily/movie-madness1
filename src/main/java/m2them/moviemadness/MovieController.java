5ac8fe816bc944cbca0252b664ba71ec21504f7a
SanjayBheenuck <48724324+SanjayBheenuck@users.noreply.github.com>
2019-04-23 12:19:59 +0100

    movies controller (movie page)

--- src/main/java/m2them/moviemadness/MovieController.java
+++ src/main/java/m2them/moviemadness/MovieController.java
@@ -6,42 +6,63 @@ import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 
 @Controller()
 public class MovieController {
 
     @GetMapping({"/index", "/"})
-    public String moviesIndex(Model model){
+    public String moviesIndex(Model model) {
         ArrayList<Movie> movies = Database.getMovies(Database.connectDB());
         movies.sort(Movie.releaseComparator);
         model.addAttribute("movies", movies);
         model.addAttribute("sortType", new SortType(3));
         return "index";
-        }
+    }
 
-        @RequestMapping("/")
-        public String movieSort(Model model, @ModelAttribute("sortType") SortType sortType) {
-            ArrayList<Movie> movies = Database.getMovies(Database.connectDB());
-            switch (sortType.getSortType()) {
-                case 1:
-                    movies.sort(Movie.titleComparator);
-                    break;
-                case 2:
-                    movies.sort(Movie.titleComparator.reversed());
-                    break;
-                case 3:
-                    movies.sort(Movie.releaseComparator);
-                    break;
-                case 4:
-                    movies.sort(Movie.releaseComparator.reversed());
-                    break;
-            }
-            model.addAttribute("movies", movies);
-            return "index";
+    @RequestMapping("/")
+    public String movieSort(Model model, @ModelAttribute("sortType") SortType sortType) {
+        ArrayList<Movie> movies = Database.getMovies(Database.connectDB());
+        switch (sortType.getSortType()) {
+            case 1:
+                movies.sort(Movie.titleComparator);
+                break;
+            case 2:
+                movies.sort(Movie.titleComparator.reversed());
+                break;
+            case 3:
+                movies.sort(Movie.releaseComparator);
+                break;
+            case 4:
+                movies.sort(Movie.releaseComparator.reversed());
+                break;
         }
+        model.addAttribute("movies", movies);
+        return "index";
+    }
+
+
+    //request mapping
+    @RequestMapping("/movies")
+    public String movieSort(Model model, @ModelAttribute("movie") Movie movie) {
+
+
+                ArrayList<Actor> actors = Database.getActorsByMovie(movie);
+                ArrayList<Review> reviews = Database.getReviewsByMovie(movie);
+        model.addAttribute("actors", actors);
+        model.addAttribute("reviews", reviews);
+        model.addAttribute("movie", movie);
+
+
+        return "movie";
+
 
     }
+
+
+    }
+
 
 
 
